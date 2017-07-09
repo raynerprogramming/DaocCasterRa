6c04d32cfd3098e493045a18379a91a15889462c
raynerprogramming <raynerprogramming@gmail.com>
2017-07-08 20:53:14 -0500

    prototype

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-var app = angular.module('app',['ui.router','slick'])
+var app = angular.module('app',['ui.router'])
 //CONTROLLERS
-.controller('HomeController',function($scope){
+.controller('HomeController',function($scope,$http){
 	$scope.data='Home';
 	$scope.imgsrc1 = "images/background.jpg";
+
+	$http.get("/data/radmgcost.xml",
+                    {
+                        transformResponse: function (cnv) {
+                            var x2js = new X2JS();
+                            var aftCnv = x2js.xml_str2json(cnv);
+                            return aftCnv;
+                        }
+                    })
+            .then(function (success) {
+                $scope.data=success.data["data-set"].record;
+            },function (error){
+            	console.log(error)
+            });
 
 	$('#sidenav-overlay').trigger('click');
 	$scope.init = function(){
@@ -14,6 +28,16 @@ var app = angular.module('app',['ui.router','slick'])
 	angular.element(document).ready(function () {
 		$scope.init();
 	});
+
+	$scope.calcBestRA = function(){
+		if(!($scope.numRA>0))
+			return;
+		available=_($scope.data).filter(function(data){
+		 return data.cost <= $scope.numRA}
+		 );
+		$scope.best=_(available).max(function (data){ return data.dmg});
+	}
+
 })
 //STATE CONFIG
 app.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
